f3a805e59d488292dd36ccac70dd3c74364014d7
alexjuliansmith <alexjuliansmith@gmail.com>
2015-09-14 17:37:02 +0100

    PopularityLeaguePOC

--- PopularityLeague.java
+++ PopularityLeague.java
@@ -10,6 +10,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -28,11 +29,95 @@ public class PopularityLeague extends Configured implements Tool {
         int res = ToolRunner.run(new Configuration(), new PopularityLeague(), args);
         System.exit(res);
     }
+    
+    public static String readHDFSFile(String path, Configuration conf) throws IOException{
+        Path pt=new Path(path);
+        FileSystem fs = FileSystem.get(pt.toUri(), conf);
+        FSDataInputStream file = fs.open(pt);
+        BufferedReader buffIn=new BufferedReader(new InputStreamReader(file));
+
+        StringBuilder everything = new StringBuilder();
+        String line;
+        while( (line = buffIn.readLine()) != null) {
+            everything.append(line);
+            everything.append("\n");
+        }
+        return everything.toString();
+    }
 
     @Override
     public int run(String[] args) throws Exception {
-        // TODO
-    }
+    	  Job job = Job.getInstance(this.getConf(), "Popularity League");
+          job.setOutputKeyClass(Text.class);
+          job.setOutputValueClass(IntWritable.class);
 
-    // TODO
+          job.setMapOutputKeyClass(Text.class);
+          job.setMapOutputValueClass(IntWritable.class);
+
+          job.setMapperClass(LinkCountMap.class);
+          job.setReducerClass(LinkRankReduce.class);
+
+          FileInputFormat.setInputPaths(job, new Path(args[0]));
+          FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+          job.setJarByClass(PopularityLeague.class);
+          return job.waitForCompletion(true) ? 0 : 1;
+    }
+    
+    public static class LinkCountMap extends Mapper<Object, Text, Text, IntWritable> {
+        List<String> leagueMembers;
+        
+        @Override
+        protected void setup(Context context) throws IOException,InterruptedException {
+            Configuration conf = context.getConfiguration();
+            String leagueMembersPath = conf.get("league");
+            this.leagueMembers = Arrays.asList(readHDFSFile(leagueMembersPath, conf).split("\n"));
+        }
+
+            
+            @Override public void map(Object key, Text value, Context context)  throws IOException, InterruptedException {
+               StringTokenizer t  = new StringTokenizer(value.toString(), ": ");
+               Text page = new Text(t.nextToken());
+               //if (leagueMembers.contains(page)) context.write(page, new IntWritable(0));
+               while (t.hasMoreTokens()) {
+                 Text link = new Text(t.nextToken());
+                 if (leagueMembers.contains(link.toString())) context.write(link, new IntWritable(1));
+               }
+            }
+      }
+
+      public static class LinkRankReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+    	  List<String> leagueMembers;
+    	  TreeSet<Pair<Integer, String>> rankings;
+          
+    	  @Override
+          protected void setup(Context context) throws IOException,InterruptedException {
+              Configuration conf = context.getConfiguration();
+              String leagueMembersPath = conf.get("league");
+              this.leagueMembers = Arrays.asList(readHDFSFile(leagueMembersPath, conf).split("\n"));
+          }
+    	  
+          @Override public void reduce(Text key, Iterable<IntWritable> values, Context context)  throws IOException, InterruptedException {
+        	  String page = key.toString();
+        	  if (leagueMembers.contains(page)) {
+	            int sum = 0; 
+	            for (IntWritable value : values) {
+	               sum += value.get();
+	            }
+	            rankings.add(Pair.of(sum, page));
+        	}
+          }
+
+		@Override
+		protected void cleanup(Context context)	throws IOException, InterruptedException {
+			int rank = 0; int lastCount = -1;
+			for (Pair<Integer, String> page : rankings) {
+				context.write(new Text(page.second), new IntWritable(page.first));
+				rank++;
+			}
+		}
+          
+          
+
+      }
 }
